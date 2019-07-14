04324de695cf147c97b02f2e7d6df1a2ad2dec5e
john horta <john.horta@yahoo.com>
2019-07-14 19:53:00 -0400

    mass location now added contact info

--- Routes/api-routes.js
+++ Routes/api-routes.js
@@ -9,7 +9,7 @@ const axios = require("axios");
 router.get("/api/products", async (req, res) => res.json((await db.Product.findAll({}))));
 router.get("/api/doctors", async (req, res) => {
     console.log("keys", keys)
-    axios.get(`https://api.betterdoctor.com/2016-03-01/doctors?specialty_uid=psychiatrist&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=5&user_key=${keys}`).then(function (result) {
+    axios.get(`https://api.betterdoctor.com/2016-03-01/doctors?specialty_uid=psychiatrist&location=ma-boston&skip=0&limit=10&user_key=${keys}`).then(function (result) {
         res.json(result.data.data);
     })
 })
