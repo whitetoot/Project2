bce829d6f095ee5b757ac3dd5f0caa71ffd67cc6
john horta <john.horta@yahoo.com>
2019-07-14 21:42:17 -0400

    made changes

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,2 @@
 DROP DATABASE IF EXISTS Pryia_db;
-CREATE DATABASE `Pryia_db`.`events` (
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `title` VARCHAR(255),
-    `description` TEXT,
-    `location` TEXT,
-    `contact` TEXT,
-    `url` VARCHAR(255),
-    `start` DATETIME,
-    `end` DATETIME,
-    PRIMARY KEY (`id`),
-    INDEX idx_start (`start`),
-    INDEX idx_end (`end`)
-) ENGINE=INNODB DEFAULT CHARSET=UTF8 COLLATE=UTF8_UNICODE_CI;()
+CREATE DATABASE Pryia_db;
